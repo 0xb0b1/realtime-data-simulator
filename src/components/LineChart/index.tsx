ae03a7a787ebefcf74da0cca8d60230b1089e629
0xb0b1 <vcente82@gmail.com>
2021-03-26 10:53:10 -0300

    ...

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -1,71 +1,16 @@
-import React, { Component } from "react";
-import { Bar, Line, Pie } from "react-chartjs-2";
+import { ReactNode } from "react";
 
-class Chart extends Component {
-  constructor(props: {} | Readonly<{}>) {
-    super(props);
-    this.state = {
-      chartData: props.chartData,
-    };
-  }
-
-  static defaultProps = {
-    displayTitle: true,
-    displayLegend: true,
-    legendPosition: "right",
-    location: "City",
-  };
-
-  render() {
-    return (
-      <div className="chart">
-        <Bar
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Largest Cities In " + this.props.location,
-              fontSize: 25,
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition,
-            },
-          }}
-        />
-
-        <Line
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Largest Cities In " + this.props.location,
-              fontSize: 25,
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition,
-            },
-          }}
-        />
-
-        <Pie
-          data={this.state.chartData}
-          options={{
-            title: {
-              display: this.props.displayTitle,
-              text: "Largest Cities In " + this.props.location,
-              fontSize: 25,
-            },
-            legend: {
-              display: this.props.displayLegend,
-              position: this.props.legendPosition,
-            },
-          }}
-        />
-      </div>
-    );
-  }
+interface LineChartProps {
+  children: ReactNode;
 }
 
-export default Chart;
+function LineChart({ children }: LineChartProps) {
+  return (
+    <>
+      <h1>LineChart</h1>
+      {children}
+    </>
+  );
+}
+
+export default LineChart;
